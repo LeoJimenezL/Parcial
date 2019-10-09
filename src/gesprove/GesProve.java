a728c1d774c28bc2fd0d4ebbb4861f310b66552d
leonardo <leonardo@ubuntu-X442URR>
2019-10-09 10:57:22 -0500

    Parcial 2do corte

---
+++ src/gesprove/GesProve.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gesprove;
+
+import vista.GUI_Principal;
+
+/**
+ *
+ * @author leonardo
+ */
+public class GesProve 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        GUI_Principal prin = new GUI_Principal();
+        prin.setLocationRelativeTo(null);
+        prin.setVisible(true);
+    }
+    
+}
